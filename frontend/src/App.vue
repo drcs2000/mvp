937ca47fc3e050e4b0db805f9ba3995a470b8edc
Davi Rangel <davirangelcamara@hotmail.com>
2024-07-03 18:57:24 -0300

    Adicionando o navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <v-app>
     <v-main>
+      <nav-bar />
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import navBar from "./components/navBar.vue";
+
 export default {
   name: "App",
 
   data: () => ({
     //
   }),
+  components: {
+    navBar,
+  },
 };
 </script>
---
+++ frontend/src/components/navBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-container>
+      <v-row align="center" justify="space-between">
+        <v-col cols="auto">
+          <v-img src="@/assets/logo.png" contain width="50" height="50"></v-img>
+        </v-col>
+
+        <v-col cols="auto" class="text-center">
+          <v-btn text>
+            <v-icon color="red">mdi-circle</v-icon>
+            Ao vivo
+          </v-btn>
+          <v-btn text>Campeonatos</v-btn>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-btn outlined color="secondary">Logar</v-btn>
+          <v-btn color="secondary">
+            Registrar
+            <v-icon right>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped>
+.v-app-bar {
+  background-color: #21475b !important; /* Ajuste a cor conforme necessário */
+}
+.text-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
