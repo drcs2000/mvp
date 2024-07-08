e11e4ae9d4b56b408fa0a1f50248ed3445653362
Davi Rangel <davirangelcamara@hotmail.com>
2024-07-08 14:55:52 -0300

    Creating Live Game showing in the home page section

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid fill-height class="pa-0">
-    <div class="video-container">
+    <div class="parallax-container">
       <video autoplay muted loop id="background-video">
         <source :src="videoSrc" type="video/mp4" />
         Your browser does not support the video tag.
@@ -13,6 +13,21 @@
         {{ $t("welcome-title-part3") }}
       </h1>
       <p>{{ $t("welcome-message") }}</p>
+    </div>
+    <div class="live-games">
+      <h2>{{ $t('live') }}</h2>
+      <div class="game">
+        <span class="team">Fluminense</span>
+        <span class="score">1</span>
+        <span class="minute">79'</span>
+        <span class="score">1</span>
+        <span class="team">Internacional</span>
+      </div>
+      <div class="pagination">
+        <span class="dot" :class="{ active: true }"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div>
     </div>
   </v-container>
 </template>
@@ -40,9 +55,10 @@ export default {
 }
 
 #background-video {
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: auto;
   object-fit: cover;
+  opacity: 0.8; /* Increased opacity */
 }
 
 .content {
@@ -52,6 +68,7 @@ export default {
   transform: translate(-50%, -50%);
   color: white;
   text-align: center;
+  z-index: 1;
 }
 
 .content h1 {
@@ -64,5 +81,58 @@ export default {
 
 .highlight {
   color: #c6de41;
+}
+
+.live-games {
+  background-color: #153b44;
+  color: white;
+  padding: 20px;
+  text-align: center;
+  width: 100%;
+}
+
+.live-games h2 {
+  margin-bottom: 20px;
+}
+
+.game {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+}
+
+.game .team {
+  font-size: 1.2rem;
+  margin: 0 10px;
+}
+
+.game .score {
+  font-size: 2rem;
+  margin: 0 10px;
+}
+
+.game .minute {
+  font-size: 1rem;
+  color: #c6de41;
+}
+
+.pagination {
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+}
+
+.dot {
+  height: 10px;
+  width: 10px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  margin: 0 5px;
+}
+
+.dot.active {
+  background-color: #c6de41;
 }
 </style>
