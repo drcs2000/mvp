4ab9a58ab599929a6f405db0713a0692be628c01
drcs2000 <davirangelcamara@hotmail.com>
2025-09-19 16:28:14 -0300

    feat: Prepara para deploy na Render

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -1,29 +1,43 @@
+// src/database/data-source.ts
+
 import 'reflect-metadata';
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { Pool } from '../entities/pool.entity';
 import { PoolParticipant } from '../entities/pool-participant.entity';
-import { Match } from '../entities/match.entity'
-import { Championship } from '../entities/championship.entity'
-import { Standings } from '../entities/standings.entity'
-import { Bet } from '../entities/bet.entity'
-import { ChampionshipStandingRule } from '../entities/championship-standing-rule.entity'
-import { Head2Head } from '../entities/h2h.entity'
-import { Invitation } from '../entities/invitation.entity'
+import { Match } from '../entities/match.entity';
+import { Championship } from '../entities/championship.entity';
+import { Standings } from '../entities/standings.entity';
+import { Bet } from '../entities/bet.entity';
+import { ChampionshipStandingRule } from '../entities/championship-standing-rule.entity';
+import { Head2Head } from '../entities/h2h.entity';
+import { Invitation } from '../entities/invitation.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+
+  url: process.env.DATABASE_URL,
+
+  ssl: process.env.NODE_ENV === 'production' 
+    ? { rejectUnauthorized: false } 
+    : false,
 
   synchronize: false,
   logging: true,
 
-  entities: [User, Pool, PoolParticipant, Match, Championship, Standings, Bet, ChampionshipStandingRule, Head2Head, Invitation],
+  entities: [
+    User,
+    Pool,
+    PoolParticipant,
+    Match,
+    Championship,
+    Standings,
+    Bet,
+    ChampionshipStandingRule,
+    Head2Head,
+    Invitation,
+  ],
 
   migrations: ["src/database/migrations/*.{ts,js}"],
 });
